534e3fb1b4afc80ffef73b5fc64dc5e6b0f68e48
mokrinskiy <artyommokrinskiy@gmail.com>
2023-12-03 22:21:23 +0700

    fix deploy 5

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,8 +1,8 @@
 echo "BUILD START"
+mkdir -p staticfiles_build
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py makemigrations
 python3 manage.py loaddata data.json
 echo "BUILD END"
